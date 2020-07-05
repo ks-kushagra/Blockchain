e214a020e3b3485efccd8609e3f4b87c9fa964a3
Kushagra Singh <kushagra.cse.11498@gmail.com>
2020-07-05 15:26:08 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,3 +1,8 @@
+//To handle HTTP POST request in Express.js version 4 and above, you need to install middleware module called body-parser.
+//body-parser extract the entire body portion of an incoming request stream and exposes it on req.body.
+//The middleware was a part of Express.js earlier but now you have to install it separately.
+//This body-parser module parses the JSON, buffer, string and URL encoded data submitted using HTTP POST request. Install body-parser using NPM as shown below.
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const  Blockchain = require('../blockchain');
